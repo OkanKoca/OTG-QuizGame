96957c94026b65c5b0e8a4c34e1a4b0b29ad8f75
Okan Koca <okankoca67@gmail.com>
2024-05-10 15:22:12 +0300

    build error solved

--- Assets/Scripts/AnswerButton.cs
+++ Assets/Scripts/AnswerButton.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine.UI;
 using JetBrains.Annotations;
 
